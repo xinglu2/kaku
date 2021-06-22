27909c727d3c887ac4d6075e691b28d1e22661f9
luluxing <2787363534@qq.com>
2021-06-22 22:46:19 +0800

    change height

---
+++ src/main.js
@@ -0,0 +1,98 @@
+const PC = window.matchMedia('(min-width:500px)')
+const sidebarHeight = () => {
+    if (PC.matches) {
+        $('.sidebar').height(window.screen.availHeight)
+    }
+}
+sidebarHeight()
+const mediaQuery = window.matchMedia('(max-width:500px)')
+$('.menu').click(() => {
+    $('.sidebar-main').css('display', 'block')
+    $('.content').css('filter', 'blur(0.8rem)')
+})
+const $sidebarMain = $('.sidebar-main')
+const moveChange = () => {
+    if (mediaQuery.matches) {
+        $('.content').on('click', () => {
+            $sidebarMain.css('display', 'none')
+            $('.content').css('filter', 'blur(0)')
+        })
+        $('.sidebar-item').on('click', () => {
+            $sidebarMain.css('display', 'none')
+            $('.content').css('filter', 'blur(0)')
+        })
+
+    }
+}
+moveChange()
+
+const $lastList = $('.lastList')
+const $lastLi = $lastList.find('li.last')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [
+    { logo: 'A', logoType: 'text', url: 'https://www.acfun.cn' },
+    { logo: 'B', logoType: 'text', url: 'https://www.bilibili.com/' },
+    { logo: 'G', logoType: 'text', url: 'https://www.google.com/' }
+
+]
+const removePre = (url) => {
+    return url.replace("http://", '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '')
+}
+const render = () => {
+    $lastList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li id="touchArea">
+            <div class="site">
+                <div class="logo">${node.logo[0]}</div>
+                <div class="link">${removePre(node.url)}</div>
+                <div class="close">
+                    <svg class="icon">
+                        <use xlink:href="#icon-delete"></use>
+                    </svg>
+                </div>
+            </div>
+    </li>`).insertBefore($lastLi)
+        $li.on('click', (e) => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()
+            hashMap.splice(index, 1)
+            render()
+        })
+    })
+}
+render()
+$('.addButton')
+    .on('click', () => {
+        let url = window.prompt('请问你要添加的网址是:')
+        // console.log(url)
+        const firstLetter = removePre(url)[0].toUpperCase()
+        if (url.indexOf('http') !== 0) {
+            url = 'https://' + url
+        }
+        hashMap.push({
+            logo: firstLetter,
+            logoType: 'text',
+            url: url
+        });
+        render()
+    })
+
+window.onbeforeunload = () => {
+    const string = JSON.stringify(hashMap)
+    localStorage.setItem('x', string)//就是一个key，一个value,key可以随便写
+}
+$(document).on('keypress', (e) => {
+    console.log(e)
+    const { key } = e
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
+            window.open(hashMap[i].url)
+        }
+    }
+})
